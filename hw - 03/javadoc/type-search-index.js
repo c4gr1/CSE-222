4a68bcb9d6d4a9191150eefb39676482ad142a33
c4gr1 <104283350+c4gr1@users.noreply.github.com>
2024-07-08 01:56:59 +0300

    Delete hw - 03/javadoc directory

--- hw - 03/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Device"},{"p":"<Unnamed>","l":"Headphone"},{"p":"<Unnamed>","l":"Inventory"},{"p":"<Unnamed>","l":"Keyboard"},{"p":"<Unnamed>","l":"Laptop"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Smartphone"},{"p":"<Unnamed>","l":"TV"}]
